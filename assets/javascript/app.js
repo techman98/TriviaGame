4f7b85f10b56369b4837ab8bd603f22076f91ca8
Techman <wizmar.tyler@gmail.com>
2019-01-04 19:41:53 -0600

    Finally working, took for ever my ass but yay.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,43 @@
+$(document).ready(function() {
 var questions = $(".question");
 var numCorrect = 0;
 var numIncorrect = 0;
-var number = 60;
-var seconds;
+var seconds = 60;
+var intervalId;
+
+// var answer = 
 
 function run() {
-  seconds = setInterval(decrement, 1000);
+  $(".timer").html("<h2>" + seconds + "</h2>");
+  intervalId = setInterval(decrement, 1000);
 }
 
 function decrement() {
-  number--;
-  $(".timer").html("<h2>" + number + "</h2>");
-  if (number === 0) {
+  seconds--;
+  $(".timer").html("<h2>" + seconds + "</h2>");
+  if (seconds === 0) {
     stop();
-    $(".timer").text("<h2>Time Up!");
-    for (var i = 0; i < questions.length; i++) {
-
-
-        // console.log(event);
-        // console.log(event.target.class);
-        // if (event.target.className === "right") {
-        //     numCorrect++
-        //     console.log(numCorrect);
-        // } else {
-        //     numIncorrect++
-        //     console.log(numIncorrect);
-        // }
-    }
+    $(".timer").text("Time Up!");
+    checkAnswers();
   }
 }
 
 function stop() {
-  clearInterval(seconds);
+  clearInterval(intervalId);
+
+}
+
+function checkAnswers() {
+  for (var i = 1; i <= questions.length; i++) {
+
+    var selector = `input[name=answer${i}]:checked`;
+    var checked = ($(selector).attr("value"));
+    if (checked === "right") numCorrect++
+    else numIncorrect++
+
+  }
+  $(".questions").html("<h2>" + "Correct: " + numCorrect + "<br>" + "Incorrect: " + numIncorrect + "</h2>");
 }
 
 run();
+})
